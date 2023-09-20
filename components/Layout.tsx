7510a8d475b8ea25e05c3bd90217ad205f1b87de
Everyhongcha <80094147+everyt@users.noreply.github.com>
2023-09-20 19:07:01 +0900

    1

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,10 @@
-'use client';
-
-import { useSession } from 'next-auth/react';
-
 import NavBar from '@/components/NavBar/NavBar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { data: session } = useSession();
   return (
-    <>
-      {!session ? (
-        <div className='flex h-screen w-screen items-center justify-center'>{children}</div>
-      ) : (
-        <div className='flex h-screen w-screen justify-between'>
-          <NavBar />
-          {children}
-        </div>
-      )}
-    </>
+    <div className='flex h-screen w-screen justify-between'>
+      <NavBar />
+      {children}
+    </div>
   );
 }
