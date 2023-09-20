f973cf5990368cb3c837a72cb669a154d3587560
Everyhongcha <80094147+everyt@users.noreply.github.com>
2023-09-20 19:36:07 +0900

    1

--- components/Login.tsx
+++ components/Login.tsx
@@ -7,42 +7,44 @@ import { signIn, useSession } from 'next-auth/react';
 export default function Login() {
   const { data: session } = useSession();
   return (
-    <div className='top-1/3 mb-36 flex flex-col items-center border-2'>
-      <div className='flex w-[300px] flex-col items-center'>
-        <Image
-          className='mt-8 h-[53px] w-[150px]'
-          src='/svg/Instagram-text-light.svg'
-          alt='Instagram'
-          width={150}
-          height={53}
-        />
-      </div>
-      <div className='flex flex-col items-center'>
-        <p className='z-20 bg-white p-3'>SNS 아이디로 로그인</p>
-        <div className='absolute z-10 mt-6 w-[250px] border-t-2' />
-      </div>
-      <div className='flex h-[70px] flex-row justify-center'>
-        {[
-          [0, 'naver', () => signIn('naver', { callbackUrl: '/' })],
-          [1, 'kakao', () => signIn('kakao', { callbackUrl: '/' })],
-          [2, 'google', () => signIn('google', { callbackUrl: '/' })],
-          [3, 'github', () => signIn('github', { callbackUrl: '/' })],
-        ].map(([key, brand, func]) => (
-          <button
-            key={key as number}
-            className='mx-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-stone-100 hover:opacity-80 hover:shadow-lg'
-            onClick={func as React.MouseEventHandler}
-          >
-            <Image //
-              className='h-7 w-7'
-              src={key === 0 ? '/svg/naver.svg' : `https://authjs.dev/img/providers/${brand}.svg`}
-              alt={brand as string}
-              height={26}
-              width={26}
-            />
-            {session && session.user?.name}
-          </button>
-        ))}
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <div className='mb-36 flex flex-col items-center border-2'>
+        <div className='flex w-[300px] flex-col items-center'>
+          <Image
+            className='mt-8 h-[53px] w-[150px]'
+            src='/svg/Instagram-text-light.svg'
+            alt='Instagram'
+            width={150}
+            height={53}
+          />
+        </div>
+        <div className='flex flex-col items-center'>
+          <p className='z-20 bg-white p-3'>SNS 아이디로 로그인</p>
+          <div className='absolute z-10 mt-6 w-[250px] border-t-2' />
+        </div>
+        <div className='flex h-[70px] flex-row justify-center'>
+          {[
+            [0, 'naver', () => signIn('naver', { callbackUrl: '/' })],
+            [1, 'kakao', () => signIn('kakao', { callbackUrl: '/' })],
+            [2, 'google', () => signIn('google', { callbackUrl: '/' })],
+            [3, 'github', () => signIn('github', { callbackUrl: '/' })],
+          ].map(([key, brand, func]) => (
+            <button
+              key={key as number}
+              className='mx-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-stone-100 hover:opacity-80 hover:shadow-lg'
+              onClick={func as React.MouseEventHandler}
+            >
+              <Image //
+                className='h-7 w-7'
+                src={key === 0 ? '/svg/naver.svg' : `https://authjs.dev/img/providers/${brand}.svg`}
+                alt={brand as string}
+                height={26}
+                width={26}
+              />
+              {session && session.user?.name}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
