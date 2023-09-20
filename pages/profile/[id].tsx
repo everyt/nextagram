915c84d8c5af0a47eac4c9925af9f9089e8709ba
everyt <everytea.time@gmail.com>
2023-09-20 16:56:29 +0900

    for bug

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -4,5 +4,7 @@ import Profile from '@/components/Profile/Profile';
 
 export default function ProfilePage() {
   const router = useRouter();
+  console.log(router.query.slug);
+  console.table(router.query.slug);
   return <Profile id={router.query.slug as string} />;
 }
